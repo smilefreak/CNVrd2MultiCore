f06ad81bf64dacdc52ffaa97ff928f35d45cd0ef
James Boocock <smilefreak@gmx.com>
2013-09-13 06:09:02 +0000

    process should be working properlynow

--- amazon_coordinates/process_sample.py
+++ amazon_coordinates/process_sample.py
@@ -1,6 +1,9 @@
 from optparse import OptionParser
 from boto.s3.connection import S3Connection
 import os
+import logging
+import time
+import progressbar
 #
 #  Creates coordinates files for each file in the 1000 genomes data set
 #
@@ -8,10 +11,50 @@ import os
 #
 #
 
+pbar = None
+logging.basicConfig(filename="sample.log", level=logging.INFO)
+
+import pysam
+
+def read_bams(bam_file):
+    return pysam.SamFile(bam_file,'r')
+
+def write_coordinates_and_length(bam_file,output_file):
+    with open(output_file,'wb') as f:
+       return 0  
+
 def open_1kg_connection(access_key,secret_key):
     conn=S3Connection(access_key,secret_key)
     buck=conn.get_bucket('1000genomes')
     return buck
+
+def progress_callback(current,total):
+    try:
+        pbar.update(current)
+    except AssertionError, e:
+        logging.error(e)
+
+def download_file(bam_file,key):
+    # get_file_size
+    global pbar
+    size  = key.size
+    print(key)
+    print(key.size)
+    logging.info("File Size " + str(size))
+    widgets = [
+        unicode(bam_file, errors='ignore').encode('utf-8'), ' ',
+        progressbar.FileTransferSpeed(),
+        ' <<<', progressbar.Bar(), '>>> ',
+        progressbar.Percentage(), ' ', progressbar.ETA()
+    ]
+    pbar = progressbar.ProgressBar(widgets=widgets, maxval=size)
+    pbar.start()
+    try:
+        key.get_contents_to_filename(bam_file,cb=progress_callback,num_cb=10000)
+    except Exception, e:
+        logging.error("Failed to Download " + bam_file)
+        logging.error(e)
+    pbar.finish()
 
 def main():
     parser = OptionParser()
@@ -33,10 +76,19 @@ def main():
         line= f.readline() 
         for line in f:
             if( i ==0 ):
+                line = line.strip()
                 key = buck.get_key(line.strip())
-                bam_file = os.path.join(working_dir,os.path.basename(line)))
-                key.get_contents_to_filename(bam_file)
-                
+                bam_file = os.path.join(working_dir,os.path.basename(line))
+                download_file(bam_file,key)
+                # Read bam into R get readlength + position as a 64 bit interger.# 
+                read_bams = read_bam(bam_file)
+
+                # This is where it gets a bit obscure 
+                # first byte = chromosome number in ASCII
+                # second byte = read length
+                # third byte = position start of the read mapped to 
+                output = os.path.basename(bam_file) + ".cnvb"
+                write_coordinates_and_length(bam_file,output)
                 i = i + 1
             else:
                 break
