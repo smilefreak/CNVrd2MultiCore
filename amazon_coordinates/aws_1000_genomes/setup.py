74f1c35962fdbbd1aaed5467888e67ae858cc8cd
James Boocock <smilefreak@gmx.com>
2013-09-17 09:14:04 +0000

    Added new_python_server

---
+++ amazon_coordinates/aws_1000_genomes/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="aws_1000_genomes",
+    version="0.1",
+    author = "James Boocock",
+    author_email = "smilefreak@gmx.com",
+    packages =find_packages(),
+    zipSafe=False,
+    entry_points = {
+        'console_scripts': [
+            'bam_server = aws1kg.http_bam_server:main',
+            'process_bam = aws1kg.process_sample:main'
+        ]
+        }
+    )
