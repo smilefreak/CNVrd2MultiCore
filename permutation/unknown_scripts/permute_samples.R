d22bd558765d26bf0b00c7dd2b397475343cb92f
smilefreak <smilefreak@gmx.com>
2014-04-09 00:14:58 +1200

    Addedsomescripts that were used for teh first analysis unknown origin

---
+++ permutation/unknown_scripts/permute_samples.R
@@ -0,0 +1,17 @@
+# Read in and calculate CNVrd2 on bam files
+
+
+
+require(getopt)
+require(CNVrd2)
+require(parrelel)
+
+# read in all the data
+
+# permute for every sample
+
+# save dataset
+
+# save informative plots 
+
+# save image.
